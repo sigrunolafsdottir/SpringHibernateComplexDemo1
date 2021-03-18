fdc83d136064b1ea74d8ba6dd4edc5766674bc2f
Sigrun Olafsdottir <solafsdottir@gmail.com>
2021-03-18 10:20:53 +0100

    Added static index.html

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -3,13 +3,15 @@ package com.example.demo.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //only for demoing testing
 
-@RestController
+@Controller
 @RequestMapping(path = "/")
 public class HelloController {
 
@@ -17,12 +19,14 @@ public class HelloController {
 
     @GetMapping(path="/")
     public String HelloWorld() {
-        log.info("SomeOne looked at our web site!!!");
-        return "Hello World!";
+        //log.info("SomeOne looked at our web site!!!");
+        //return "Hello World!";
+        return "index.html";
     }
 
     @GetMapping(path="/es")
-    public String HolaMundo() {
+    public @ResponseBody
+    String HolaMundo() {
         return "Hola Mundo!";
     }
 
